cc5f8181abe551f6d7f4e714af9a82812420b702
Mudarken <91074680+SolexBear@users.noreply.github.com>
2026-02-25 18:35:42 +0100

    Add files via upload

---
+++ KlimaX_Doors/Data/T4Door/config.cpp
@@ -0,0 +1,161 @@
+class CfgPatches
+{
+	class KlimaX_T4Door
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data",
+			"DZ_Structures_Signs"
+		};
+	};
+};
+class CfgVehicles
+{
+	class HouseNoDestruct;
+	class Land_KlimaX_T4Door: HouseNoDestruct
+	{
+		scope=1;
+		model="KlimaX_Doors\Data\T4Door\KlimaX_T4Door.p3d";
+		class Doors
+		{
+			class doorOpen
+			{
+				displayName="Twin Doors 1";
+				component="doorOpen";
+				soundPos="doorOpen_action";
+				animPeriod=3.7;
+				initPhase=0;
+				initOpened=0;
+				soundOpen="KlimaX_T4Door";
+				soundClose="KlimaX_T4Door";
+				soundLocked="doorMetalSlideLargeRattle";
+				soundOpenABit="doorMetalSlideLargeOpenABit";
+			};
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=1000;
+				};
+			};
+			class GlobalArmor
+			{
+				class Projectile
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+				class Melee
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+				class FragGrenade
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+			};
+			class DamageZones
+			{
+				class twin1
+				{
+					class Health
+					{
+						hitpoints=2000;
+						transferToGlobalCoef=0;
+					};
+					componentNames[]=
+					{
+						"door1"
+					};
+					fatalInjuryCoef=-1;
+					class ArmorType
+					{
+						class Projectile
+						{
+							class Health
+							{
+								damage=3;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class Melee
+						{
+							class Health
+							{
+								damage=5;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class FragGrenade
+						{
+							class Health
+							{
+								damage=3;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+					};
+				};
+			};
+		};
+	};
+};
